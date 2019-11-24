40d4668ba6bcce76f4d0ed003b0686095c444f87
MindShare Learning <vijay@mind-share.in>
2019-11-24 18:55:12 +0530

    Added Material UI based component

--- matui/matui-basic/src/webparts/basicForm/components/BasicForm.tsx
+++ matui/matui-basic/src/webparts/basicForm/components/BasicForm.tsx
@@ -1,25 +1,47 @@
 import * as React from 'react';
 import styles from './BasicForm.module.scss';
 import { IBasicFormProps } from './IBasicFormProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Container from "@material-ui/core/Container";
 
 export default class BasicForm extends React.Component<IBasicFormProps, {}> {
+  public state : any = {};
+
+  constructor(props: IBasicFormProps) {
+    super(props);
+
+    this.state = {
+      username : "vijay",
+      password : "dsd434geg23e",
+      server: 1
+    };
+  }
   public render(): React.ReactElement<IBasicFormProps> {
     return (
-      <div className={ styles.basicForm }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <>
+        <Container >
+          <FormControl>
+            <Typography component="div" style={{ fontWeight: "bold" }} >Login Form</Typography><br />
+
+            <TextField id="userid" label="Username" value={ this.state.username } /><br /><br />
+            <TextField id="password" label="Password" type="password" value={ this.state.password } /> <br /> <br />
+
+            <Select placeholder="Server">
+              <MenuItem value={1} selected>India</MenuItem>
+              <MenuItem value={2}>USA</MenuItem>
+              <MenuItem value={3}>UK</MenuItem>
+              <MenuItem value={4}>France</MenuItem>
+            </Select><br /><br />
+            <Button variant="contained" color="primary">SUBMIT</Button>
+          </FormControl>
+        </Container>
+      </>
     );
   }
 }
